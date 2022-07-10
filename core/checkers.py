170461816e9cb55cfb11d83e181b9b4f1d90106c
owati <mofiyinfoluwaowati@gmail.com>
2022-07-10 21:19:07 +0100

    Tested the generic checkers function for list with custom types

--- core/checkers.py
+++ core/checkers.py
@@ -2,7 +2,7 @@
 
 import abc
 from types import GenericAlias
-from core.exceptions import ParamsDoesNotMatchError
+from exceptions import ParamsDoesNotMatchError
 
 from exceptions import InvalidGenericAliasError
 
@@ -10,6 +10,10 @@ class Checker(abc.ABC):
     @abc.abstractmethod
     def is_type(value) -> bool:
         pass
+    
+    @staticmethod
+    def convert_to_type_string(str_val) -> str:
+        return f"<class '{str_val}'>"
 
 
 class GenericAliasChecker(Checker):
@@ -38,23 +42,34 @@ class GenericAliasChecker(Checker):
 
 
     def is_type(self,value) -> bool | None:
-        if str(type(value)) == self.main_type:
-            types_list = self.other_types.split(' ,')
+        print(str(type(value)))
+        
+        if str(type(value)) == self.convert_to_type_string(self.main_type):
+            types_list = self.other_types.split(', ')
+
+            print(types_list)
 
             if self.main_type == 'list':
                 # when the value is of type list
                 if len(types_list) == 1:
                     # when the parametized types of the list is just one
                     for _value in value:
-                        if type(_value) != types_list[0]:
-                            error_msg = f''
+                        if str(type(_value)) != self.convert_to_type_string(types_list[0]):
+                            error_msg = f'The one of the items of this list is not of type {types_list[0]}'
+                            raise ParamsDoesNotMatchError(error_msg)
 
-                if (len(value) != len(types_list)):
+                elif (len(value) != len(types_list)):
+                    # If the lenght don't match when it is greated that one
                     error_msg = 'The length of the value does not match the length of the type specified'
                     raise ParamsDoesNotMatchError(error_msg)
 
-            for types in self.other_types.split(','):
-                pass
+                else:
+                    # When the 
+                    for _ in range(len(value)):
+                        if str(type(value[_])) != self.convert_to_type_string(types_list[_]):
+                            error_msg = f'The value of index {_} is not of type {types_list[_]}'
+                            raise ParamsDoesNotMatchError(error_msg)
+
         else:
             error_msg = f'the value is not of type {self.main_type}'
             raise ParamsDoesNotMatchError(error_msg)
@@ -62,3 +77,4 @@ class GenericAliasChecker(Checker):
 
 if __name__ == '__main__':
     check = GenericAliasChecker(list[int, str, int])
+    check.is_type([9,'poo',3])
